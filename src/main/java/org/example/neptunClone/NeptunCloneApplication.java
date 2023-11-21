4cdd73b75120e73acfda666177cf460b8e61c9d1
Mark Kavalecz <kiskav12@gmail.com>
2023-11-21 12:41:32 +0100

    test

--- src/main/java/org/example/neptunClone/NeptunCloneApplication.java
+++ src/main/java/org/example/neptunClone/NeptunCloneApplication.java
@@ -8,6 +8,7 @@ public class NeptunCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeptunCloneApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
