cbf2b7a03c00e222fac7654038fe37a1cacca4e1
Mark Kavalecz <kiskav12@gmail.com>
2023-11-21 12:09:38 +0100

    Initial commit

---
+++ src/test/java/org/example/neptunClone/NeptunCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.neptunClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptunCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
