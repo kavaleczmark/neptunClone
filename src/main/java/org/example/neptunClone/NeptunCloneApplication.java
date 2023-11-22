9853226154f9b95c39a2548df546255b7c16f0f2
Mark Kavalecz <kiskav12@gmail.com>
2023-11-22 20:17:39 +0100

    created basic html sites

--- src/main/java/org/example/neptunClone/NeptunCloneApplication.java
+++ src/main/java/org/example/neptunClone/NeptunCloneApplication.java
@@ -8,7 +8,6 @@ public class NeptunCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeptunCloneApplication.class, args);
-		System.out.println("Hello");
 	}
 
 }
