2617a9a0931548ffb3d0be08cdc5aec5b4e9efe6
Mark Kavalecz <kiskav12@gmail.com>
2023-12-01 00:59:36 +0100

    added ClassroomControllerTest unittests

--- src/test/java/tests/ClassroomControllerTest.java
+++ src/test/java/tests/ClassroomControllerTest.java
@@ -9,11 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -31,18 +31,87 @@ public class ClassroomControllerTest {
 
     @Test
     public void testAddClassroom() {
-        // Arrange
         Classroom classroom = new Classroom(1, "B.111", 30);
         when(classroomRepository.insertClassroom(classroom)).thenReturn(true);
         ClassroomService classroomService = new ClassroomService(classroomRepository);
-
-        // Act
         boolean result = classroomService.addClassroom(classroom);
-
-        // Assert
         assertTrue(result);
         verify(classroomRepository, times(1)).insertClassroom(classroom);
     }
 
+    @Test
+    public void testGetAllClassroom() throws SQLException {
+        List<Classroom> classrooms = Arrays.asList(
+                new Classroom(1, "B.111", 100),
+                new Classroom(2, "B.113", 250)
+        );
+        when(classroomRepository.getClassroom()).thenReturn(classrooms);
+        ClassroomService classroomService = new ClassroomService(classroomRepository);
+        List<Classroom> result = classroomService.getAllClassroom();
+        assertEquals(classrooms.size(), result.size());
+    }
 
+    @Test
+    public void testGetClassroomById() throws SQLException {
+        int id = 1;
+        Classroom classroom = new Classroom(id, "B.111", 30);
+        when(classroomRepository.getClassroomById(id)).thenReturn(classroom);
+        ClassroomService classroomService = new ClassroomService(classroomRepository);
+        Classroom result = classroomService.getClassroomById(id);
+        assertEquals(id, result.getId());
+    }
+
+    @Test
+    public void testGetClassroomByName() throws SQLException {
+        String name = "B.111";
+        Classroom classroom = new Classroom(1, name, 30);
+        when(classroomRepository.getClassroomByName(name)).thenReturn(classroom);
+        Classroom result = classroomRepository.getClassroomByName(name);
+        assertEquals(name, result.getName());
+    }
+    @Test
+    public void testGetClassroomByPlaces() throws SQLException {
+        int places = 30;
+        Classroom classroom = new Classroom(1, "B.111", places);
+        when(classroomRepository.getClassroomByPlaces(places)).thenReturn(classroom);
+        Classroom result = classroomRepository.getClassroomByPlaces(places);
+        assertEquals(places, result.getSpaces());
+    }
+    @Test
+    public void testInsertClassroom_Successful() {
+        Classroom classroom = new Classroom(1, "B.111", 30);
+        when(classroomService.addClassroom(classroom)).thenReturn(true);
+        ResponseEntity<Void> result = classroomController.insertClassroom(classroom);
+        assertEquals(HttpStatus.NO_CONTENT.value(), result.getStatusCodeValue());
+    }
+    @Test
+    public void testInsertClassroom_Failure() {
+        Classroom classroom = new Classroom(1, "B.111", 30);
+        when(classroomService.addClassroom(classroom)).thenReturn(false);
+        ResponseEntity<Void> result = classroomController.insertClassroom(classroom);
+        assertEquals(HttpStatus.NOT_MODIFIED, result.getStatusCode());
+    }
+    @Test
+    public void testUpdateClassroom_Successful() throws SQLException {
+        int id = 1;
+        Classroom classroom = new Classroom(id, "B.111", 100);
+        Classroom updatedClassroom = new Classroom(id, "Updated Classroom", 40);
+        when(classroomService.getClassroomById(id)).thenReturn(classroom);
+        when(classroomService.updateClassroom(classroom)).thenReturn(true);
+        ResponseEntity<Void> result = classroomController.updateClassroom(updatedClassroom, id);
+        assertEquals(HttpStatus.OK.value(), result.getStatusCodeValue());
+        assertEquals(updatedClassroom.getName(), classroom.getName());
+        assertEquals(updatedClassroom.getSpaces(), classroom.getSpaces());
+    }
+
+    @Test
+    public void testUpdateClassroom_Failure() throws SQLException {
+        int id = 1;
+        Classroom classroom = new Classroom(id, "B.111", 100);
+        Classroom updatedClassroom = new Classroom(id, "Updated Classroom", 40);
+        when(classroomService.getClassroomById(id)).thenReturn(classroom);
+        when(classroomService.updateClassroom(classroom)).thenReturn(false);
+        ResponseEntity<Void> result = classroomController.updateClassroom(updatedClassroom, id);
+        assertEquals(HttpStatus.NOT_MODIFIED.value(), result.getStatusCodeValue());
+    }
 }
